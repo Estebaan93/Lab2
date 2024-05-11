6a5c2dff0b615217695a26b75aa2b9a62bd44f99
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2024-05-11 19:28:31 -0300

    Practica

---
+++ PUG/Ejercicios/animales.js
@@ -0,0 +1,9 @@
+//Array para el ejercicio 4
+const animales=[
+	{animal: 'Perro', color: '#234421', tienePelo: true, tienePatas: true},
+	{animal: 'Loro', color: '#00ff23', tienePelo: true, tiene4Patas: false },
+	{animal: 'Pez',color:'#ffaaa6', tienePelo: false, tiene4Patas: false}
+	];
+
+
+module.exports=animales;
--- PUG/Ejercicios/compilePug.js
+++ PUG/Ejercicios/compilePug.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const animales= require('./animales');
+
 
 const app = express();
-const port = 3002;
 
-const pugFile = './views/Ej3.pug'; // Nombre de tu archivo Pug
+
+const pugFile = './views/Ej5.pug'; // Nombre de tu archivo Pug
 const outputFile = './public/index.html'; // Nombre del archivo HTML generado
+
+//Carpeta src de img
+app.use(express.static('public/src'));
+console.log(animales);
 
 // Compilar el archivo Pug
 const compiledFunction = require('pug').compileFile(pugFile);
 
 // Generar HTML compilado
-const html = compiledFunction();
+const html = compiledFunction({animales});
 
 // Escribir el resultado HTML en un archivo
 fs.writeFileSync(outputFile, html, 'utf8');
@@ -20,11 +26,8 @@ fs.writeFileSync(outputFile, html, 'utf8');
 // Servir el archivo HTML estático
 app.use(express.static(path.join(__dirname, '/public')));
 
-// app.get('/', (req, res)=>{
-//   res.send()
-// })
-
 // Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+const PUERTO = process.env.PUERTO || 3002;
+app.listen(PUERTO, () => {
+  console.log(`Servidor escuchando en http://localhost:${PUERTO}`);
 });
